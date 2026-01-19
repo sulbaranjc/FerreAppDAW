24e4d48dbfda26fec642783e55a9f7760dfe3cda
Juan Carlos Sulbarán <sulbaranjc@gmail.com>
2026-01-19 09:17:25 +0100

    Agregar archivos de configuración y conexión a la base de datos

---
+++ app/database.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+import mysql.connector
+from typing import List, Dict, Any, cast
+from mysql.connector.cursor import MySQLCursorDict  # opción C si la prefieres
+
+# Carga .env desde la raíz
+load_dotenv(find_dotenv())
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST", "localhost"),        # <— corregidos nombres
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", ""),
+        database=os.getenv("DB_NAME", "ferreapp"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        charset="utf8mb4"
+    )
+
